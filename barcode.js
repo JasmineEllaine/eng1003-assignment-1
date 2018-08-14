28a62f73654eea2fb5a4c87e7bd491f0607f139e
Jasmine Banares <jasminebanares@dyn-118-139-54-248.its.monash.edu.au>
2018-08-14 15:16:25 +1000

    added skeleton files

---
+++ barcode.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function decodeBarcodeFromAreas(areas)
+{
+    let result = {
+        barcode: "123456", 
+        message: "No middle guard", 
+        checksumValid: false
+    };
+
+    return result;
+}
+
